505ecf5b18b90e24647100c850673685abf82944
LucasCastro <lucascastro_94@hotmail.com>
2023-01-17 17:15:28 -0300

    end project

--- src/main/java/alunosapi/com/AlunosAPI/controllers/StudentController.java
+++ src/main/java/alunosapi/com/AlunosAPI/controllers/StudentController.java
@@ -2,11 +2,14 @@ package alunosapi.com.AlunosAPI.controllers;
 
 import alunosapi.com.AlunosAPI.dto.request.StudentDTO;
 import alunosapi.com.AlunosAPI.dto.response.MessageResponseDTO;
+import alunosapi.com.AlunosAPI.exception.StudentNotFoundException;
 import alunosapi.com.AlunosAPI.services.StudentService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/students")
@@ -20,4 +23,37 @@ public class StudentController {
     public MessageResponseDTO create(@RequestBody @Valid StudentDTO studentDTO) {
         return studentService.create(studentDTO);
     }
+
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public StudentDTO findById(@PathVariable Long id) throws StudentNotFoundException {
+        return studentService.findById(id);
+    }
+
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<StudentDTO> listAll() {
+        return studentService.listAll();
+    }
+
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public MessageResponseDTO update(@PathVariable Long id, @RequestBody @Valid StudentDTO studentDTO) throws StudentNotFoundException{
+        return studentService.update(id, studentDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) throws StudentNotFoundException {
+        studentService.delete(id);
+    }
+
+
+
+
+
+
 }
--- src/main/java/alunosapi/com/AlunosAPI/services/StudentService.java
+++ src/main/java/alunosapi/com/AlunosAPI/services/StudentService.java
@@ -4,9 +4,13 @@ import alunosapi.com.AlunosAPI.dto.mapper.StudentMapper;
 import alunosapi.com.AlunosAPI.dto.request.StudentDTO;
 import alunosapi.com.AlunosAPI.dto.response.MessageResponseDTO;
 import alunosapi.com.AlunosAPI.entities.Student;
+import alunosapi.com.AlunosAPI.exception.StudentNotFoundException;
 import alunosapi.com.AlunosAPI.repositories.StudentRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -23,6 +27,42 @@ public class StudentService {
 
         return messageResponseDTO;
     }
+
+    public StudentDTO findById(Long id) throws StudentNotFoundException {
+        Student student = studentRepository.findById(id)
+                .orElseThrow(() -> new StudentNotFoundException(id));
+
+        return studentMapper.toDTO(student);
+    }
+
+    public List<StudentDTO> listAll() {
+        List<Student> people = studentRepository.findAll();
+        return people.stream()
+                .map(studentMapper::toDTO)//para cada cada um converte para DTO
+                .collect(Collectors.toList()); // joga para uma lista
+    }
+
+
+    public MessageResponseDTO update(Long id, StudentDTO personDTO) throws StudentNotFoundException {
+        studentRepository.findById(id)
+                .orElseThrow(() -> new StudentNotFoundException(id)); // caso não acha essa pessoa lance a exceção
+
+        Student updatedPerson = studentMapper.toModel(personDTO);
+        Student savedPerson = studentRepository.save(updatedPerson);
+
+        MessageResponseDTO messageResponse = createMessageResponse("Person successfully updated with ID ", savedPerson.getId());
+
+        return messageResponse;
+    }
+
+    public void delete(Long id) throws StudentNotFoundException {
+        studentRepository.findById(id)
+                .orElseThrow(() -> new StudentNotFoundException(id));
+
+        studentRepository.deleteById(id);
+    }
+
+
 
     private MessageResponseDTO createMessageResponse(String s, Long id)
     {
